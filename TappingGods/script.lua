e1605c3471c928eb786dcb42ab3d0eb066a4a5ef
Ezekiel <78991517+zekgt@users.noreply.github.com>
2021-05-15 00:41:45 +0800

    Update script.lua

--- TappingGods/script.lua
+++ TappingGods/script.lua
@@ -1,4 +1,5 @@
 -- Game Link : https://www.roblox.com/games/5346447287
+game:GetService("ReplicatedFirst")["CC_Lookout"].ClientLog:remove()
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/forumsLib/main/source.lua")))()
 local forums = Library.new("Tapping Gods")
 
