9e4f4386be788e138a369f119935b05e2816cdcb
Ezekiel <78991517+zekgt@users.noreply.github.com>
2021-05-17 12:36:42 +0800

    Update Script.lua

--- IdentityFraud/Script.lua
+++ IdentityFraud/Script.lua
@@ -27,21 +27,21 @@ end)
 maze2:addButton("Secret Door ESP", function()
     for i,v in pairs(game.Workspace:FindFirstChild("Door")) do
         if v.ClassName == 'Part' and v.Parent.Name == 'Hedge Maze' then
-        local BillboardGui = Instance.new('BillboardGui') -- Makes Billboardgui
-        local TextLabel = Instance.new('TextLabel') -- makes text label
+        local BillboardGui = Instance.new('BillboardGui')
+        local TextLabel = Instance.new('TextLabel')
         
-        BillboardGui.Parent = v.Parent -- what the billboardgui goes into
-        BillboardGui.AlwaysOnTop = true -- if its on top or not
-        BillboardGui.Size = UDim2.new(0, 100, 0, 100) -- size of it
+        BillboardGui.Parent = v.Parent
+        BillboardGui.AlwaysOnTop = true
+        BillboardGui.Size = UDim2.new(0, 100, 0, 100)
         BillboardGui.StudsOffset = Vector3.new(0,2,0)
         
-        TextLabel.Parent = BillboardGui -- putting textlabel into billboardgui
-        TextLabel.BackgroundColor3 = Color3.new(244,244,244) -- color
-        TextLabel.BackgroundTransparency = 0.6 -- transparency
-        TextLabel.Size = UDim2.new(1, 0, 1, 0) -- size
-        TextLabel.Text = "Secret Door" -- what the label says
-        TextLabel.TextColor3 = Color3.new(15, 15, 15) -- color
-        TextLabel.TextScaled = true -- if the text is scaled or not
+        TextLabel.Parent = BillboardGui
+        TextLabel.BackgroundColor3 = Color3.new(244,244,244)
+        TextLabel.BackgroundTransparency = 0.6
+        TextLabel.Size = UDim2.new(1, 0, 1, 0)
+        TextLabel.Text = "Secret Door"
+        TextLabel.TextColor3 = Color3.new(15, 15, 15)
+        TextLabel.TextScaled = true
         TextLabel.Font = "Gotham"
     end
     end
@@ -92,21 +92,21 @@ end)
 section1:addButton("Door ESP", function()
     for i,v in pairs(game.Workspace:GetDescendants()) do
         if v.ClassName == 'Part' and v.Parent.Name == 'Doors' then
-        local BillboardGui = Instance.new('BillboardGui') -- Makes Billboardgui
-        local TextLabel = Instance.new('TextLabel') -- makes text label
+        local BillboardGui = Instance.new('BillboardGui')
+        local TextLabel = Instance.new('TextLabel')
         
-        BillboardGui.Parent = v.Parent -- what the billboardgui goes into
-        BillboardGui.AlwaysOnTop = true -- if its on top or not
-        BillboardGui.Size = UDim2.new(0, 100, 0, 100) -- size of it
+        BillboardGui.Parent = v.Parent 
+        BillboardGui.AlwaysOnTop = true
+        BillboardGui.Size = UDim2.new(0, 100, 0, 100)
         BillboardGui.StudsOffset = Vector3.new(0,2,0)
         
-        TextLabel.Parent = BillboardGui -- putting textlabel into billboardgui
-        TextLabel.BackgroundColor3 = Color3.new(0.294117, 0.286274, 0.286274) -- color
-        TextLabel.BackgroundTransparency = 0.6 -- transparency
-        TextLabel.Size = UDim2.new(1, 0, 1, 0) -- size
-        TextLabel.Text = "Door" -- what the label says
-        TextLabel.TextColor3 = Color3.new(45, 45, 45) -- color
-        TextLabel.TextScaled = true -- if the text is scaled or not
+        TextLabel.Parent = BillboardGui 
+        TextLabel.BackgroundColor3 = Color3.new(0.294117, 0.286274, 0.286274)
+        TextLabel.BackgroundTransparency = 0.6
+        TextLabel.Size = UDim2.new(1, 0, 1, 0)
+        TextLabel.Text = "Door"
+        TextLabel.TextColor3 = Color3.new(45, 45, 45)
+        TextLabel.TextScaled = true
         TextLabel.Font = "Gotham"
     end
     end
