b35ca8171345f4024b8f98f2d3797ac6f190bf80
Ezekiel <78991517+zekgt@users.noreply.github.com>
2021-07-07 09:24:58 +0800

    Update main.lua

--- ClickerSim/main.lua
+++ ClickerSim/main.lua
@@ -17,25 +17,29 @@ local section1 = page1:addSection("Main")
 local section2 = page2:addSection("Misc")
 
 local firstToggle = section1:addToggle("Auto Click", nil , function(v)
-    getgenv().farm = v;
-    if not getgenv().farm then return end
-    while wait() do
-        if getgenv().farm then
-            game:GetService("Workspace").Events.AddClick:FireServer()
+    getgenv().farm = v; -- Synapse X's getgenv(), this can be optional
+    while wait() do -- loops the remote function that gives you clicks which is autofarm
+        if not getgenv().farm then return end -- return stops the loop function < this line stops the autofarm if v is valued at false, checks if getgenv().farm is false
+        if getgenv().farm then -- Checks if getgenv().farm is true and if it is true it will do a function at line 24
+            game:GetService("Workspace").Events.AddClick:FireServer() -- the click function
         end
     end
 end)
 
-local slider1 = section2:addSlider("Speed", 0, -100, 100, function(value)
-    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = value
+local slider1 = section2:addSlider("Speed", 0, -100, 100, function(value) -- the walkspeed slider
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = value -- walkspeed will be modified 
 end)
 
-local slider2 = section2:addSlider("Jump Power", 0, 10, 120, function(val)
-    game.Players.LocalPlayer.Character.Humanoid.JumpPower = val
+local slider2 = section2:addSlider("Jump Power", 0, 10, 120, function(val) -- the jumppoer slider
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = val -- jumppower will be modified
 end)
 
 local credits = section2:addDropdown("Credits", {"UI : GreenDeno", "doing nothing : ZekGT", "Scripting : otospro"}, function(text)
-    print(text)
+    setclipboard("https://v3rmillion.net")
+end)
+
+local uitoggle = section2:addKeybind("Toggle UI", Enum.KeyCode.RightControl, function()
+	venyx:toggle()
 end)
 
 local button1 = section2:addButton("Destroy Gui", function()
@@ -46,3 +50,7 @@ local button1 = section2:addButton("Destroy Gui", function()
     end
 end)
 
+--[[ Useless Stuff 
+      Q. When do I have to use getgenv()?
+      A. You have to use it when declaring a global like _G., unlike _G. getgenv() isn't detected since it is made by synapse
+]]
