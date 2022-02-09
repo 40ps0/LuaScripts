9fd88ded992ebbff89f064a1daf4b359a1bd38f4
40ps0 <99308236+40ps0@users.noreply.github.com>
2022-02-09 11:32:03 +0700

    Update main.lua

--- ClickerSim/main.lua
+++ ClickerSim/main.lua
@@ -1,65 +1,64 @@
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/GreenDeno/Venyx-UI-Library/main/source.lua"))()
-local venyx = library.new("test23", 5013109572)
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/zxciaz/VenyxUI/main/Reuploaded"))() --someone reuploaded it so I put it in place of the original back up so guy can get free credit.
+local Hen = library.new("HenTai HUB", 5013109572)
 
 -- themes
 local themes = {
-	Background = Color3.fromRGB(24, 24, 24),
-	Glow = Color3.fromRGB(0, 0, 0),
-	Accent = Color3.fromRGB(10, 10, 10),
-	LightContrast = Color3.fromRGB(20, 20, 20),
-	DarkContrast = Color3.fromRGB(14, 14, 14),  
-	TextColor = Color3.fromRGB(255, 255, 255)
+Background = Color3.fromRGB(24, 24, 24),
+Glow = Color3.fromRGB(0, 0, 0),
+Accent = Color3.fromRGB(10, 10, 10),
+LightContrast = Color3.fromRGB(20, 20, 20),
+DarkContrast = Color3.fromRGB(14, 14, 14),  
+TextColor = Color3.fromRGB(255, 255, 255)
 }
 
-local page1 = venyx:addPage("Main", 5012544693)
-local page2 = venyx:addPage("Misc")
-local section1 = page1:addSection("Main")
-local section2 = page2:addSection("Misc")
+-- first page
+local page = Hen:addPage("Test", 5012544693)
+local section1 = page:addSection("Section 1")
+local section2 = page:addSection("Section 2")
 
-local firstToggle = section1:addToggle("Auto Click", nil , function(v)
-    getgenv().farm = v; -- Synapse X's getgenv(), this can be optional
-    while wait() do -- loops the remote function that gives you clicks which is autofarm
-        if not getgenv().farm then return end -- return stops the loop function < this line stops the autofarm if v is valued at false, checks if getgenv().farm is false
-        if getgenv().farm then -- Checks if getgenv().farm is true and if it is true it will do a function at line 24
-            game:GetService("Workspace").Events.AddClick:FireServer() -- the click function
-        end
-    end
+section1:addToggle("Toggle", nil, function(value)
+print("Toggled", value)
+end)
+section2:addKeybind("Toggle Keybind", Enum.KeyCode.RightControl, function()
+print("Activated Keybind")
+Hen:toggle()
+end, function()
+print("Changed Keybind")
 end)
 
-local slider1 = section2:addSlider("Speed", 0, -100, 100, function(value) -- the walkspeed slider
+local slider1 = section2:addSlider("Speed", 0, 16, 500, function(value) -- the walkspeed slider
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = value -- walkspeed will be modified 
 end)
-
-local slider2 = section2:addSlider("Jump Power", 0, 10, 120, function(val) -- the jumppoer slider
+local slider2 = section2:addSlider("Jump Power Not work", 0, 10, 120, function(val) -- the jumppoer slider
     game.Players.LocalPlayer.Character.Humanoid.JumpPower = val -- jumppower will be modified
 end)
-
-local credits = section2:addDropdown("Credits", {"UI : GreenDeno", "doing nothing : ZekGT", "Scripting : otospro"}, function(text)
-    setclipboard("https://v3rmillion.net")
+section2:addButton("Rejoin",function()
+    local ts = game:GetService("TeleportService")
+    local p = game:GetService("Players").LocalPlayer
+    ts:Teleport(game.PlaceId, p)
 end)
-
-local uitoggle = section2:addKeybind("Toggle UI", Enum.KeyCode.RightControl, function()
-	venyx:toggle()
+section2:addButton("World",function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1.99999905, 0.496083677, 59, 1, 0, 0, 0, 1, 0, 0, 0, 1)
 end)
-
 local button1 = section2:addButton("Destroy Gui", function()
     for i,v in pairs(game.CoreGui:GetChildren()) do
-        if v.Name == "test23" then
+        if v.Name == "HenTai HUB" then
             v:Destroy()
         end
     end
 end)
+-- second page
+local teleport = Hen:addPage("teleport", 5012544693)
+local Warp = teleport:addSection("Warp")
+-- second page
+local theme = Hen:addPage("Theme", 5012544693)
+local colors = theme:addSection("Colors")
 
---[[ Useless Stuff 
-      Q. When do I have to use getgenv()?
-      A. You have to use it when declaring a global like _G., unlike _G. getgenv() isn't detected since it is made by synapse
-]]--
+for theme, color in pairs(themes) do -- all in one theme changer, i know, im cool
+colors:addColorPicker(theme, color, function(color3)
+Hen:setTheme(theme, color3)
+end)
+end
 
---[[#include <iostream>
-using workspace std;
-
-
-int main() {
-    cout << "GoodBye!";
-    return 0;
-}]]--
+-- load
+Hen:SelectPage(Hen.pages[1], true) -- no default for more freedom
